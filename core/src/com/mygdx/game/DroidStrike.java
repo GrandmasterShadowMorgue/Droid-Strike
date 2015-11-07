d25616981991670a606692e02d1206c4ad0fba19
Guido Arnau <guido.arnau@gmail.com>
2015-11-07 12:41:48 +0000

    Add menu and game screen

--- core/src/com/mygdx/game/DroidStrike.java
+++ core/src/com/mygdx/game/DroidStrike.java
@@ -9,19 +9,28 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class DroidStrike extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
+
+		InputProcessor ip = new InputProcessor();
+		Gdx.input.setInputProcessor(ip);
+		Gdx.input.setCatchBackKey(true);
+
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+        this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
+
+	@Override
+	public void dispose() {
+        batch.dispose();
+		img.dispose();
+    }
+
 }
