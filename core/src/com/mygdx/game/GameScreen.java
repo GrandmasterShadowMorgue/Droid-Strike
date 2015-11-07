b978adbeeb3df87bf5b62978764f4bb000c792d0
GrandmasterShadowMorgue <kavijay1997@yahoo.in>
2015-11-07 19:16:19 +0000

    Took care of a teeny weeny detail

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -72,7 +72,7 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        mapRenderer.render();
 		float ydir = Gdx.input.getAccelerometerY(), xdir = Gdx.input.getAccelerometerX();
 
         player.setDirection(ydir, -1*xdir);
